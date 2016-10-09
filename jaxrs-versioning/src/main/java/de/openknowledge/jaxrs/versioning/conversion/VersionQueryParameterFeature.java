d62df84b8f11e0157130a5279d33ed67c9add930
arne <arne@apache.org>
2016-10-09 09:51:57 +0200

    Added feature to specify version via custom query parameter

---
+++ jaxrs-versioning/src/main/java/de/openknowledge/jaxrs/versioning/conversion/VersionQueryParameterFeature.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) open knowledge GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package de.openknowledge.jaxrs.versioning.conversion;
+
+import javax.ws.rs.container.DynamicFeature;
+import javax.ws.rs.container.ResourceInfo;
+import javax.ws.rs.core.FeatureContext;
+import javax.ws.rs.ext.Provider;
+
+import de.openknowledge.jaxrs.versioning.VersionQueryParameter;
+
+/**
+ * @author Arne Limburg - open knowledge GmbH
+ */
+@Provider
+public class VersionQueryParameterFeature implements DynamicFeature {
+
+  @Override
+  public void configure(ResourceInfo resource, FeatureContext context) {
+    VersionQueryParameter versionParameter = resource.getResourceMethod().getAnnotation(VersionQueryParameter.class);
+    if (versionParameter == null) {
+      versionParameter = resource.getResourceClass().getAnnotation(VersionQueryParameter.class);
+    }
+    
+    if (versionParameter != null) {
+      context.register(new VersionQueryParameterFilter(versionParameter.value()));
+    }
+  }
+}
